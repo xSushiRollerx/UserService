c0d1de187a5a9aa598caaa6a256cf79b1d6e2509
Damocles-coder <35568707+dyltra@users.noreply.github.com>
2021-03-25 11:20:43 -0700

    added initial spring boot project

---
+++ sushibyte/sushibyte/src/main/java/com/xsushirollx/sushibyte/SushibyteApplication.java
@@ -0,0 +1,13 @@
+package com.xsushirollx.sushibyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushibyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushibyteApplication.class, args);
+	}
+
+}
