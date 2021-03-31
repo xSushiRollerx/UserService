b760eb93b5731ceb4c8673d5e1663608d700bc97
Damocles-coder <35568707+dyltra@users.noreply.github.com>
2021-03-31 16:15:26 -0700

    fixed url mapping (not tested yet)

--- sushibyte/src/main/java/com/xsushirollx/sushibyte/controllers/UserController.java
+++ sushibyte/src/main/java/com/xsushirollx/sushibyte/controllers/UserController.java
@@ -5,12 +5,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.xsushirollx.sushibyte.entities.User;
 import com.xsushirollx.sushibyte.service.UserService;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 	@Autowired
 	UserService u1;
@@ -29,7 +32,7 @@ public class UserController {
 	    }
 	}
 	
-	@GetMapping("/register")
+	@PutMapping("/register")
 	public String registerUser(HttpServletRequest request,
 			@Param("firstName") String firstName,
 			@Param("lastName") String lastName,
